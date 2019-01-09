3eff212d2abb49b67772a2182c075eb2bf709a92
rezwana rubaiya <rezwanarubaiya@gmail.com>
2019-01-09 09:05:54 -0600

    update

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Container.css";
 
-const Container = props => <div className="container">{props.character}</div>;
+const Container = props => <div className="container">{props.children}</div>;
 
 export default Container;
